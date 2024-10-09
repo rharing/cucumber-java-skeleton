a77fbd28c2aeca6bd631f555c56fca34c9702749
ronaldharing <ronaldharing@lostlemon.nl>
2024-10-09 14:10:09 +0200

    trials

--- maven/src/main/java/io/cucumber/skeleton/Belly.java
+++ maven/src/main/java/io/cucumber/skeleton/Belly.java
@@ -1,7 +1,17 @@
 package io.cucumber.skeleton;
 
 public class Belly {
+    private int cukes;
+    private Integer hoursToWait;
     public void eat(int cukes) {
+this.cukes = cukes;
+    }
 
+    public void waitHour(final Integer hoursToWait) {
+this.hoursToWait = hoursToWait;
+    }
+
+    public String growl() {
+        return "GROWWWLLLLL";
     }
 }
